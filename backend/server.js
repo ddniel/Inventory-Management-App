0c71107d3b6c5aed33562004186eda2a4aacf014
DannyBec <ddnielbec@outlook.com>
2024-05-07 10:26:25 -0400

    All product routes done

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userRoute = require("./routes/userRoute") 
+const productRoute = require("./routes/productRoute")
 const errorHandler = require("./middleWare/errorMiddleware")
 const cookieParser = require("cookie-parser")
+const path = require('path')
 
 
 const app = express()
@@ -19,9 +21,13 @@ app.use(express.json()) //Para manejar archivos .json en la aplicacion
 app.use(express.urlencoded({extended:false})) // Para handle data que viene via URL
 app.use(bodyParser.json())
 app.use(cookieParser())
+app.use(cors())
+
+app.use("/uploads", express.static(path.join(__dirname, 'uploads')))
 
 //Routes Middleware
 app.use("/api/users", userRoute)
+app.use("/api/products", productRoute)
 
 //Routes
 

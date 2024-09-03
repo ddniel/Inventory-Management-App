8f1c1ef8d87f0b3db0e5927e62e57da946484f93
DannyBec <ddnielbec@outlook.com>
2024-09-03 19:44:07 -0400

    updated cors url

--- backend/server.js
+++ backend/server.js
@@ -3,61 +3,56 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const userRoute = require("./routes/userRoute") 
-const productRoute = require("./routes/productRoute")
-const contactRoute = require('./routes/contactRoute')
-const errorHandler = require("./middleWare/errorMiddleware")
-const cookieParser = require("cookie-parser")
-const path = require('path')
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
-
-const app = express()
+const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-
 //Middlewares
 
-app.use(express.json()) //Para manejar archivos .json en la aplicacion
-app.use(express.urlencoded({extended:false})) // Para handle data que viene via URL
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(cors({
-    origin: ["http://localhost:5173", "https://inventory-manager.vercel.app"],
-    credentials: true
+app.use(express.json()); //Para manejar archivos .json en la aplicacion
+app.use(express.urlencoded({ extended: false })); // Para handle data que viene via URL
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://inventory-management-ixnby2a7a-dannys-projects-47c441c7.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
-}))
-
-app.use("/uploads", express.static(path.join(__dirname, 'uploads')))
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //Routes Middleware
-app.use("/api/users", userRoute)
-app.use("/api/products", productRoute)
-app.use("/api/contactus", contactRoute)
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
 
 //Routes
 
-app.get("/", (req, res)=>{
-    res.send("Hola Danito")
-})
+app.get("/", (req, res) => {
+  res.send("Hola Danito");
+});
 
 //Error Middleware
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 //Conect to DataBase and start server
 mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-
-        app.listen(PORT, () =>{
-            console.log(`Server running on port ${PORT}`)
-        })
-
-    })
-    .catch((err) => console.log(err))
-
-
-  
-
-
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));

fbd76a3e2c0217f752c4e2554ffca7ea5ef8c2dc
DannyBec <ddnielbec@outlook.com>
2024-09-04 11:49:32 -0400

    Partioned cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -53,6 +53,7 @@ const registerUser = asyncHandler(async (req, res) => {
     expires: new Date(Date.now() + 1000 * 86400), //1 day
     sameSite: "none",
     secure: true,
+    partitioned: true,
   });
 
   if (user) {
@@ -105,6 +106,7 @@ const loginUser = asyncHandler(async (req, res) => {
     expires: new Date(Date.now() + 1000 * 86400), //1 day
     sameSite: "none",
     secure: true,
+    partitioned: true,
   });
 
   if (user && passwordIsCorrect) {

15d35a5bcb55a9735876a9357655c8517cd63074
DannyBec <ddnielbec@outlook.com>
2024-09-04 12:19:09 -0400

    updated frontend url

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://inventory-management-app-sand.vercel.app",
+      "https://inventory-management-app-s4r6.onrender.com",
     ],
     credentials: true,
   })

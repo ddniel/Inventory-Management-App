ebcf13bb9dacbd78e1d771b6c1a4ac431047c68e
DannyBec <ddnielbec@outlook.com>
2024-09-14 11:03:19 -0400

    fixed getloggedinstatus

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -142,7 +142,9 @@ const loginStatus = asyncHandler(async (req, res) => {
 
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
-
+    if (verified) {
+      return res.json(true);
+    }
     return res.json(true);
   } catch (error) {
     return res.json(false);
